12031f0210d30f61d921de10c19dd602b53be3d3
Brittany <brittanyabarnard@gmail.com>
2020-11-19 16:14:49 -0500

    initial pseudocode

---
+++ script.js
@@ -0,0 +1,45 @@
+//create namespace
+
+//make ajax call
+    //url
+    //method
+    //datatype
+
+//search engine:
+//form
+    //text field
+        //params(product type & category)
+        //stretch goal params: price/price range, product brand & tags
+    //submit button
+        //prevent default
+    //label for text field
+
+//take input
+
+//get input (event listener)
+    //listen for submit
+    //store value in text field in a variable
+    //pass variable through relevant param
+
+    
+//display input on page
+//print search on page param(stretch)
+//display results with tags (stretch)
+    //create html for the results
+        //img
+        //price
+        //name
+
+//STRETCH:
+//create <p> tag
+    //set a starting number of stock
+    //decrease stock
+//display number in each result
+//buy now button
+
+//initialize app
+
+//make the doc ready
+    //call init
+
+
