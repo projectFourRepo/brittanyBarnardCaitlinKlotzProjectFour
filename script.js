b20c923e77679024e05ef40d62bb7c51081efbe5
Brittany <brittanyabarnard@gmail.com>
2020-11-19 19:08:13 -0500

    mvp

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 //create namespace
+const makeupApp = {};
 
 //make ajax call
-    //url
-    //method
-    //datatype
+makeupApp.getMakeup = function() {
+    return $.ajax ({
+        //url
+        url: "https://makeup-api.herokuapp.com/api/v1/products.json",
+        //method
+        method: "GET",
+        //datatype
+        dataType: "json"
+    })
+}
+
+//store promise
+makeupApp.makeupPromise = () => {
+    //create a constant to store our ajax call
+    const makeupProducts = makeupApp.getMakeup();
+    console.log("eyeliner", makeupProducts);
+
+    //get promise back
+
+    $.when(makeupProducts).then((result) => {
+        console.log(result);
+    })
+} 
+
+//get input (event listener)
+makeupApp.displayMakeup = function() {
+    //listen for submit
+    $("#searchButton").on("click", function(e){
+        e.preventDefault();
+        //store value in text field in a variable
+        const searchInput = $("#makeupSearch").val();
+        console.log(searchInput);
+        //pass variable through relevant param
+        //compare search input available params
+            //to the lowercase value
+            makeupApp.makeupPromise();
+    })
+
+}
 
 //search engine:
 //form
@@ -16,10 +53,6 @@
 
 //take input
 
-//get input (event listener)
-    //listen for submit
-    //store value in text field in a variable
-    //pass variable through relevant param
 
     
 //display input on page
@@ -38,8 +71,18 @@
 //buy now button
 
 //initialize app
+makeupApp.init = function() {
+    // console.log("initialized:)");
+    makeupApp.getMakeup();
+    makeupApp.displayMakeup();
+}
 
 //make the doc ready
+$(function() {
+    // console test
+    console.log("I'm ready!");
     //call init
+    makeupApp.init();
+});
 
 
